bf571f90abfbcfb35470146e8749cb13050be72e
moncayo16 <160347878+moncayo16@users.noreply.github.com>
2024-03-12 21:18:35 +0000

    cambio img cambio code

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="mi app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= "http://imagenes.4ever.eu/abstractos/fondo-de-color-rosa-225918#google_vignette" className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> Hola Clase Pro.
         </p>
-        <a
-          className="App-link"
+        <a>
+          className="Claseprogramacion"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer
           Learn React
         </a>
       </header>
